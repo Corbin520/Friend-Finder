e51e1fd705650ebbbcedde0d95ad70449a2839d1
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-05-08 21:39:46 -0600

    added pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,14 @@
 module.exports = [
     { // index[0]
       name: 'Napoleon Dynamite',
-      photo: 'placeholder',
+      photo: 'https://www.google.com/search?q=napoleon+dynamite&rlz=1C5CHFA_enUS833US833&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiq5MWowo3iAhWTKH0KHWH1AtcQ_AUIDigB&biw=1440&bih=789#imgrc=3DWwq64ZUgaRgM:',
       scores: [3, 5, 2, 5, 1, 3, 2, 4, 4, 1] 
     },  
     { // index[1]
       name: 'Donovan Mitchell',
-      photo: 'placeholder',
+      photo: 'https://www.google.com/search?q=donovan+mitchell&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj3rdzfwo3iAhVQGDQIHZ0ZAlwQ_AUIDygC&biw=1440&bih=789&dpr=2#imgdii=tZTM9z64H11ZhM:&imgrc=VJo7kD__yIqZfM:',
       scores: [1, 5, 4, 1, 3, 2, 2, 4, 5, 1]
     }, 
 ];
+
+// Napoleon is the best match.
